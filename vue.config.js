b1e719fffe0ac5bd560298574a4ccb6f2d36cd7c
Isaah <isaaholiveira15@gmail.com>
2024-05-08 22:26:54 -0300

    feat: mfe vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig(
+  // code snippets, this is going to tell webpack to use the system in the library target 
+  {
+    configureWebpack: {
+      output: {
+        libraryTarget: "system",
+      }
+    },
+  },
+  {
+    transpileDependencies: true,
+  }
+);
